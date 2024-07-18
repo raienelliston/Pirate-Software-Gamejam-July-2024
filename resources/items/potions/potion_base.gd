9821e06bece266b796edf9fbee75f264d1308b08
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-18 07:24:59 -0700

    tried to fix color not called correcly and wrote the dragging code

--- resources/items/potions/potion_base.gd
+++ resources/items/potions/potion_base.gd
@@ -4,5 +4,22 @@ extends Node2D
 
 @onready var label = $RigidBody2D/Label
 
+var selected = false
+
 func _ready() -> void:
-	label.text = base_class.potion_color
+	#label.text = str(base_class.potion_color)
+	pass
+
+func _process(delta):
+	if selected:
+		followMouse()
+		
+func followMouse():
+	position = get_global_mouse_position()
+	
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if Input.is_action_pressed("secondary_action"):
+			selected = true
+		else:
+			selected = false
