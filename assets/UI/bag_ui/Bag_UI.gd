c73754c833866d181ef6c216c6cb7b0dbc8d4586
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-18 16:05:27 -0700

    setup bag menu animations

--- assets/UI/bag_ui/Bag_UI.gd
+++ assets/UI/bag_ui/Bag_UI.gd
@@ -1,3 +1,29 @@
-class_name BagUI
-extends Container
+extends Node
 
+enum state {opened, closed, undefined}
+
+@export var starting_state: state = state.opened
+
+var current_state: state = starting_state
+
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	pass
+
+func _on_tab_button_input_event(viewport, event, shape_idx):
+	if Input.is_action_pressed("primary_action"):
+		if current_state == state.opened:
+			close_menu()
+		if current_state == state.closed:
+			open_menu()
+
+func open_menu():
+	current_state = state.undefined
+	animation_player.play("menu_open")
+	current_state = state.opened
+	
+func close_menu():
+	current_state = state.undefined
+	animation_player.play("menu_close")
+	current_state = state.closed
