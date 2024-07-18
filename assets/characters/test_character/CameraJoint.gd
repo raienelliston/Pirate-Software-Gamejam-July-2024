94ae8a644e779b7e9e989a080e3dcfd25c928e66
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-17 18:07:23 -0700

    setup character movement and camera connection

---
+++ assets/characters/test_character/CameraJoint.gd
@@ -0,0 +1,4 @@
+extends SpringArm3D
+
+func _on_ready() -> void:
+	pass
--- assets/characters/test_character/test_character.gd
+++ assets/characters/test_character/test_character.gd
@@ -1,1 +1,19 @@
 extends CharacterBody3D
+
+@export var speed := 10.0
+@export var jump_height := 10.0
+@export var time_to_apex := 0.7
+@export var time_to_fall := 0.4
+@export var gravity := 50.0
+
+func _onready() -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	var veloctiy = Vector3.ZERO
+	
+	velocity.x = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
+	velocity.z = Input.get_action_strength("move_up") - Input.get_action_strength("move_down")
+	
+	
+	move_and_slide()
