902b886e0e3c4ee630ec6ce6de0cbd4e37624f86
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-19 09:35:54 -0700

    setup the spawning with the new item wrapper, added ways for it to connect to resource values of an item and get dimensions and such

--- resources/tools/item.gd
+++ resources/tools/item.gd
@@ -1,10 +1,17 @@
 @tool
 extends Node3D
 
-@export var itemSprite: Texture
+@export var itemTexture: Texture
+@export var width: int
+@export var height: int
 
 @onready var item_sprite = $ItemSprite
-@onready var collision_shape_2d = $Area2D/CollisionShape2D.shape.rectangle
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
 
 func _ready():
-	pass
+	item_sprite.texture = itemTexture
+	
+	# Set collision_shape
+	var shape = RectangleShape2D.new()
+	shape.size = Vector2(width, height)
+	collision_shape_2d.set_shape(shape)
