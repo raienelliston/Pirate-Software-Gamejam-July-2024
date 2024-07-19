6010e75db0615cf3b7eb9190fcc838ff6e644a1a
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-19 16:01:22 -0700

    tried to setup dynamic collision box, started on setting up dragging into the bag ui

--- assets/UI/bag_ui/Bag_UI.gd
+++ assets/UI/bag_ui/Bag_UI.gd
@@ -12,11 +12,9 @@ func _ready():
 	var current_state = state.closed
 	
 func _on_menu_body_mouse_entered():
-	print(current_state)
 	open_menu()
 
 func _on_menu_body_mouse_exited():
-	print(current_state)
 	if current_state == state.opened:
 		close_menu()
 
--- resources/tools/item.gd
+++ resources/tools/item.gd
@@ -1,18 +1,28 @@
 @tool
 extends Node3D
 
-@export var item_resource: Resource
+enum ItemStates {}
 
+@export var item_resource: Resource
 @onready var item_sprite = $ItemSprite
 @onready var collision_shape_2d = $Area2D/CollisionShape2D
 
 func _ready():
 	item_sprite.texture = item_resource["texture"]
 	
-	var width = item_sprite.texture.get_width
-	var height = item_sprite.texture.get_height
+	var width = item_sprite.texture.get_width()
+	var height = item_sprite.texture.get_height()
 	# Set collision_shape
 	print(width, height)
 	var shape = RectangleShape2D.new()
-	#shape.size = Vector2(width, height)
+	shape.size = Vector2(width, height)
 	collision_shape_2d.set_shape(shape)
+
+func _process(delta):
+	pass
+
+func changeTo2D():
+	pass
+	
+func changeTo3D():
+	pass
