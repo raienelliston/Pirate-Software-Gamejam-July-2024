47b2af525d9de00e280046f214389d9b1e09326e
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-20 00:20:52 -0700

    weird changes and trying to figure out the FSM for these items

---
+++ resources/tools/2d_items/2d_item.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var resource: Resource
+@export var texture: Texture
+#@export var starting_state: 
+
+func _ready():
+	pass
+	
+func _process(delta):
+	pass
+	
+func changeTo3D():
+	pass
---
+++ resources/tools/dragging_FSM.gd
@@ -0,0 +1,16 @@
+class_name DragFSM
+extends Node
+
+enum DragState {IDLE, CLICKED, DRAGGING, DROPPED}
+
+func Enter():
+	pass
+	
+func Exit():
+	pass
+
+func Update(_delta: float):
+	pass
+	
+func Physics_Update(_delta: float):
+	pass
