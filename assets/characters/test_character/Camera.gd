b2c517ca738cb0ab9c5a9e628b413f99244baa09
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-22 08:33:41 -0700

    attempt to get 3d mouse position fixed

--- assets/characters/test_character/Camera.gd
+++ assets/characters/test_character/Camera.gd
@@ -14,13 +14,15 @@ func get_camera_position(pos: Vector3):
 	
 func get_mouse_position():
 	var mouse_pos = get_viewport().get_mouse_position()
-	var ray_length = 1000
+	var ray_length = 10000
 	var from = project_ray_origin(mouse_pos)
 	var to = from + project_local_ray_normal(mouse_pos) * ray_length
 	var space = get_world_3d().direct_space_state
 	var ray_query = PhysicsRayQueryParameters3D.new()
 	ray_query.from = from
 	ray_query.to = to
+	ray_query.collision_mask = 1
+	ray_query.collide_with_bodies = true
 	var raycast_result = space.intersect_ray(ray_query)
 	if raycast_result == {}:
 		return
