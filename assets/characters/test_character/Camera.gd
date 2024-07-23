1918c5e9d3ef5cf12d2059f670617bcbc7132a1a
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-23 04:29:58 -0700

    fixed getting the mouse global position in 3d

--- assets/characters/test_character/Camera.gd
+++ assets/characters/test_character/Camera.gd
@@ -14,18 +14,3 @@ func _ready():
 func get_camera_position(pos: Vector3):
 	return camera_3d.unproject_position(pos)
 	
-func _physics_process(delta):
-	
-	var ray_length = 10000
-	
-	var mouse_pos = get_viewport().get_mouse_position()
-	var from = project_ray_origin(mouse_pos)
-	var to = from + project_local_ray_normal(mouse_pos) * ray_length
-	var space = get_world_3d().direct_space_state
-	var ray_query = PhysicsRayQueryParameters3D.create(from, to)
-	ray_query.collision_mask = 4
-	ray_query.collide_with_bodies = true
-	var raycast_result = space.intersect_ray(ray_query)
-	if not raycast_result == {}:
-		print(raycast_result["position"])
-		GlobalSignals.emit_signal("MouseCoordinates", raycast_result["position"])
--- assets/characters/test_character/test_character.gd
+++ assets/characters/test_character/test_character.gd
@@ -6,10 +6,30 @@ extends CharacterBody3D
 @export var time_to_fall := 0.4
 @export var gravity := 50.0
 
+@onready var camera_3d = $Camera3D
+
 func _ready() -> void:
 	add_to_group("player_character")
 	
 func _physics_process(delta: float) -> void:
+	
+	var ray_length = 10000
+	
+	var mouse_pos = get_viewport().get_mouse_position()
+	var from = camera_3d.project_ray_origin(mouse_pos)
+	var to = from + camera_3d.project_local_ray_normal(mouse_pos) * ray_length
+	var space = get_world_3d().direct_space_state
+	var ray_query = PhysicsRayQueryParameters3D.create(from, to)
+	ray_query.collision_mask = 1
+	ray_query.collide_with_bodies = true
+	ray_query.collide_with_areas = false
+	var raycast_result = space.intersect_ray(ray_query)
+	if not raycast_result == {}:
+		print(global_position + camera_3d["position"] - raycast_result["position"])
+		GlobalSignals.emit_signal("MouseCoordinates", global_position + camera_3d["position"] - raycast_result["position"])
+	
+	print(global_position)
+	
 	var veloctiy = Vector3.ZERO
 	
 	velocity.x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * speed
