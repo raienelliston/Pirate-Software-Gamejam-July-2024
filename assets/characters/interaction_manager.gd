b281aa9bf5ec81a4a9635ead2eb125f2af3b682a
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-24 02:28:45 -0700

    setup subviewport with text for tooltip and

--- assets/characters/interaction_manager.gd
+++ assets/characters/interaction_manager.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
+@onready var tooltip = $Sprite3D/SubViewport/Tooltip
 @onready var player = get_tree().get_first_node_in_group("player")
 
-var active_areas = []
+var active_areas := []
 
 func register_area(area: InteractionArea):
 	active_areas.push_back(area)
@@ -13,9 +14,15 @@ func unregister_area(area: InteractionArea):
 		active_areas.remove_at(index)
 
 func _process(delta):
-	if active_areas.size > 0 and Global.can_interact:
+	#if active_areas.size() > 0 and Global.can_interact:
+	if true:
 		active_areas.sort_custom(_sort_by_distance)
-		
+		var tooltip_text = "{primary} to use"
+		if active_areas.size() > 1:
+			tooltip_text += ", {secondary} to swap actions"
+			
+		tooltip_text = tooltip_text.format({"primary": "Primary", "secondary": "Secondary"})
+		tooltip.text = tooltip_text
 		
 func _sort_by_distance(area1, area2):
 	var area1_distance = player.global_position.distance_to(area1.global_position)
