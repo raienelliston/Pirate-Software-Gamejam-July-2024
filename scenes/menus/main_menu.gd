570fde89dd267911e6d256ef9abb6b2818b1312f
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-23 18:37:12 -0700

    setup basic menues and their functionality

---
+++ scenes/menus/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var settings_menu = $"Settings Menu"
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/home/home_scene.tscn")
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_packed(settings_menu)
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
---
+++ scenes/menus/settings_menu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_exit_settings_pressed():
+	get_tree
