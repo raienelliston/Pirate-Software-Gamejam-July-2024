85da25b5f6a8b566a38182cbf860af47567e26a6
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-24 03:44:32 -0700

    created more for the interaction manager

--- assets/characters/interaction_manager.gd
+++ assets/characters/interaction_manager.gd
@@ -1,28 +1,46 @@
 extends Node3D
 
-@onready var tooltip = $Sprite3D/SubViewport/Tooltip
+var active_areas := []
+var active_index := 0
+
+@onready var interaction_name = $Sprite3D/SubViewport/VBoxContainer/InteractionName
+@onready var interaction_description = $Sprite3D/SubViewport/VBoxContainer/InteractionDescription
+@onready var tooltip = $Sprite3D/SubViewport/VBoxContainer/Tooltip
 @onready var player = get_tree().get_first_node_in_group("player")
 
-var active_areas := []
+
+func _input(event):
+	if event.is_action_pressed("secondary_action"):
+		if active_index == active_areas.size() - 1:
+			active_index = 0
+		else:
+			active_index += 1
 
 func register_area(area: InteractionArea):
 	active_areas.push_back(area)
+	active_index += 1
 	
 func unregister_area(area: InteractionArea):
 	var index = active_areas.find(area)
 	if index != -1:
 		active_areas.remove_at(index)
+		active_index -= 1
 
 func _process(delta):
-	#if active_areas.size() > 0 and Global.can_interact:
-	if true:
+	if active_areas.size() > 0 and Global.can_interact:
 		active_areas.sort_custom(_sort_by_distance)
 		var tooltip_text = "{primary} to use"
 		if active_areas.size() > 1:
 			tooltip_text += ", {secondary} to swap actions"
 			
+		interaction_name = active_areas[active_index].interaction_name
+		interaction_description = active_areas[active_index].interaction_description
 		tooltip_text = tooltip_text.format({"primary": "Primary", "secondary": "Secondary"})
 		tooltip.text = tooltip_text
+	else:
+		interaction_name.text = ""
+		interaction_description.text = ""
+		tooltip.text = ""
 		
 func _sort_by_distance(area1, area2):
 	var area1_distance = player.global_position.distance_to(area1.global_position)
--- resources/tools/classes/interaction_area.gd
+++ resources/tools/classes/interaction_area.gd
@@ -1,2 +1,5 @@
 class_name InteractionArea
 extends Area3D
+
+@export var interaction_name: String
+@export var interaction_description: String 
