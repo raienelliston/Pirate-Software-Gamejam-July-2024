16040fdcc17ee9ad8852c4540f89013a793b086f
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-23 17:32:32 -0700

    setup 3d camera rotation and made player logic less intensive on physics process

--- assets/characters/test_character/test_character.gd
+++ assets/characters/test_character/test_character.gd
@@ -5,43 +5,61 @@ extends CharacterBody3D
 @export var time_to_apex := 0.7
 @export var time_to_fall := 0.4
 @export var gravity := 50.0
-@export var camera_rotation_speed := 50.0
+@export var camera_rotation_speed := 0.5
 
 var current_rotation: float
+var ray_length = 1000
 
 @onready var camera_3d = $CameraOrigin/Camera3D
 @onready var camera_pivot = $CameraOrigin
 
 func _ready() -> void:
 	add_to_group("player_character")
-	camera_pivot
+	if Global.can_camera_rotate:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event):
+	if event is InputEventMouse:
+		if Input.is_action_pressed("mouse_escape"):
+			var mouse_pos = get_viewport().get_mouse_position()
+			var from = camera_3d.project_ray_origin(mouse_pos)
+			var to = from + camera_3d.project_local_ray_normal(mouse_pos) * ray_length
+			var space = get_world_3d().direct_space_state
+			var ray_query = PhysicsRayQueryParameters3D.create(from, to)
+			ray_query.collision_mask = 1
+			ray_query.collide_with_bodies = true
+			ray_query.collide_with_areas = false
+			var raycast_result = space.intersect_ray(ray_query)
+			if not raycast_result == {}:
+				Global.emit_signal("MouseCoordinates", raycast_result["position"])
+	
+		# Camera Rotation
+		else:
+			if Global.can_camera_rotate:
+				rotate_y(deg_to_rad(event.relative.x * camera_rotation_speed))
+				camera_pivot.rotate_x(deg_to_rad(event.relative.y * camera_rotation_speed))
+	# Key based camera rotation
+	if Global.can_camera_rotate:
+		rotate_y(deg_to_rad((Input.get_action_strength("camera_left") - Input.get_action_strength("camera_right")) * camera_rotation_speed))
 	
 func _physics_process(delta: float) -> void:
 	
 	current_rotation = self.rotation.y
-	
-	var ray_length = 1000
-	
-	var mouse_pos = get_viewport().get_mouse_position()
-	var from = camera_3d.project_ray_origin(mouse_pos)
-	var to = from + camera_3d.project_local_ray_normal(mouse_pos) * ray_length
-	var space = get_world_3d().direct_space_state
-	var ray_query = PhysicsRayQueryParameters3D.create(from, to)
-	ray_query.collision_mask = 1
-	ray_query.collide_with_bodies = true
-	ray_query.collide_with_areas = false
-	var raycast_result = space.intersect_ray(ray_query)
-	if not raycast_result == {}:
-		Global.emit_signal("MouseCoordinates", raycast_result["position"])
-	
 	var veloctiy = Vector3.ZERO
 	
-	velocity.x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * speed * abs(cos(current_rotation))
-	velocity.z = (Input.get_action_strength("move_down") - Input.get_action_strength("move_up")) * speed * abs(cos(current_rotation))
+	# Gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 	
-	if Global.can_camera_rotate:
-		rotate_y(deg_to_rad((Input.get_action_strength("camera_left") - Input.get_action_strength("camera_right")) * camera_rotation_speed * delta))
-	
+	# Y-Plane movment
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 	
 	
 	move_and_slide()
